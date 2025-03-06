4b23e8d60a2097a42fbead5d5628274dd1356d10
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-06 17:32:58 +0600

    Update Variable.kt

--- Basics/Variable.kt
+++ Basics/Variable.kt
@@ -1,8 +1,26 @@
 // val variable_name: Data_type = Initial value
+// val keyword - Use when you expect the variable value will not change.
+// var keyword - Use when you expect the variable value can change.
 
 fun main() {
-    val count: Int = 2
-    val name: String = "Messi"
-    val salary: Double = 540.50
-    println(name + " Has a salary of "+ salary +"$")
+    
+    // val count: Int = 2
+    // val name: String = "Messi"
+    // val salary: Double = 540.50
+    
+    // val name = "Messi"
+    // val salary = 540.50
+    // var age = 90
+    
+    // println(name + " Has a salary of "+ salary +"$")
+    // println("$name has a salary of $salary$")        //both will print same line
+
+    // println(age)
+    // age = 100
+    // println(age)
+
+    val nextMeeting = "Next meeting: "
+    val date = "January 1"
+    val reminder = nextMeeting + date + " at work"
+    println(reminder)
 }
