89af154e6be85cd32955df51af1231e64442d8da
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-09 00:40:15 +0600

    Create Condition2.kt

---
+++ Basics/Condition2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val kotlin = "🙂"
+    println(kotlin)
+    
+    val time = 10
+	val greeting = if (time < 18) {
+  		"Good day."
+	} else {
+  		"Good evening."
+	}
+println(greeting)
+}
