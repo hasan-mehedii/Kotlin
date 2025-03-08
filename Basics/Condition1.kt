467512d332c2a9d1b321b4d422e910a6c1caa39d
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-09 00:38:21 +0600

    Create Condition1.kt

---
+++ Basics/Condition1.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val kotlin = "🙂"
+    println(kotlin)
+    
+    val num1 = 230
+    val num2 = 30
+    
+    if(num1 > num2){
+        println(num1)
+    }
+    else{
+        println(num2)
+    }
+}
