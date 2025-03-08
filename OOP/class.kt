6ab9b9ebdda0de65246a6b9eaf40169e53439e31
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-09 01:13:54 +0600

    Create class.kt

---
+++ OOP/class.kt
@@ -0,0 +1,14 @@
+class Student{
+    var roll = 0
+    var name = ""
+}
+
+fun main() {
+    
+    val S1 = Student()
+
+    S1.roll = 1
+    S1.name = "Mehedi"
+
+    println("We have got Roll:${S1.roll} with Name:${S1.name}")
+}
