8542aa8ff4a3b4e14de51f9f893b0ecfc220af65
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-09 00:50:59 +0600

    Create Loop2.kt

---
+++ Basics/Loop2.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val kotlin = "🙂"
+    println(kotlin)
+    
+    // While Loop
+    var i = 0
+    while(i<10){
+        println("$i Mehedi")
+        i++
+    }
+    
+    // Do While Loop
+    var j = 0
+    do{
+        println("$j Hasan")
+        j++
+    }while(j<10)
+}
